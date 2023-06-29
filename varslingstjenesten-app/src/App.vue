66640cac17be63a52572554ef4249921f7eeca7f
magnetenstad <tenstad.magne@gmail.com>
2023-06-29 14:57:40 +0200

    Remove unused components, edit css

--- varslingstjenesten-app/src/App.vue
+++ varslingstjenesten-app/src/App.vue
@@ -1,9 +1,18 @@
 <template>
-  <MapView></MapView>
+  <div class="map-container">
+    <MapView></MapView>
+  </div>
 </template>
 
 <script setup lang="ts">
 import MapView from './components/MapView.vue';
 </script>
 
-<style scoped></style>
+<style scoped>
+.map-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
