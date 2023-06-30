26ab83e370e284aa6978243aec61c351ed540924
Tobias Lømo <tobiaslomo@Tobias-sin-MacBook-Pro.local>
2023-06-30 13:38:59 +0200

    added button

---
+++ src/views/map/nowcast.vue
@@ -0,0 +1,14 @@
+<template>
+  <button @click="weatherFunction" id="weatherButton">Temp:</button>
+</template>
+
+<script lang="ts" setup>
+
+  async function weatherFunction() {
+    const response = await fetch('http://127.0.0.1:8000/met/nowcast?lat=59.9333&lon=10.7166')
+    const resJson = await response.json()
+    console.log(resJson)
+  }
+
+
+</script>
