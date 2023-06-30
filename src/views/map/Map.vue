3f91084759afe591bcc5c3d0be2479418ba69889
magnetenstad <tenstad.magne@gmail.com>
2023-06-30 14:48:05 +0200

    Add circle at user location

--- src/views/map/Map.vue
+++ src/views/map/Map.vue
@@ -5,7 +5,11 @@
 <script lang="ts" setup>
 import WebMap from '@arcgis/core/WebMap'
 import MapView from '@arcgis/core/views/MapView.js'
+import Graphic from '@arcgis/core/Graphic'
+import GraphicsLayer from '@arcgis/core/layers/GraphicsLayer'
 import { onMounted } from 'vue'
+
+const graphicsLayer = new GraphicsLayer()
 
 onMounted(() => {
   const map = new WebMap({
@@ -14,6 +18,7 @@ onMounted(() => {
       id: 'b139409c28884967a1a603695e0b478d', // https://arcg.is/1mTnbH
     },
   })
+  map.add(graphicsLayer)
 
   const view = new MapView({
     map: map,
@@ -28,6 +33,25 @@ onMounted(() => {
         center: [position.coords.longitude, position.coords.latitude],
         zoom: 12,
       })
+      const point = {
+        type: 'point',
+        longitude: position.coords.longitude,
+        latitude: position.coords.latitude,
+      }
+      const simpleMarkerSymbol = {
+        type: 'simple-marker',
+        color: [226, 119, 40], // Orange
+        outline: {
+          color: [255, 255, 255], // White
+          width: 1,
+        },
+      }
+      const pointGraphic = new Graphic({
+        // @ts-ignore
+        geometry: point,
+        symbol: simpleMarkerSymbol,
+      })
+      graphicsLayer.add(pointGraphic)
     })
   })
 })
