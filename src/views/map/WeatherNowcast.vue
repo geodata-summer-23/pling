0b7143cd0a789285b2af12a5c4e463679fb180ad
Tobias Lømo <tobias.lomo@gmail.com>
2023-07-03 13:05:50 +0200

    Added weather symbols
    Co-authored-by: Iver Sten Gustavsen <IGustavsen@users.noreply.github.com>

--- src/views/map/WeatherNowcast.vue
+++ src/views/map/WeatherNowcast.vue
@@ -1,34 +1,49 @@
 <template>
-  <div>
-    <p>Temp:  {{ temp }} {{ units ? units.air_temperature : "" }}</p>
-    <p v-if="units">Precipitation {{ precipitation }} {{ units.precipitation_amount }}</p>
-
+  <div v-if="!responseCode">
+    <p> Can't connect to the server </p>
   </div>
-  <button @click="weatherFunction" id="weatherButton">Get weather</button>
+  <div v-if="responseCode">
+    <p>Temp:  {{ temp }} {{ units ? units.air_temperature : "" }}</p>
+    <p>Precipitation {{ precipitation }} {{ units ? units.precipitation_amount : "" }}</p>
+    <img v-if="symbol" :src="`weatherSymbols/${symbol}.svg`"/>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
 
 
   const temp = ref(0)
   const precipitation = ref(0)
   const units = ref(null as any)
+  const responseCode = ref(false)
+  const symbol = ref(null)
 
-  async function weatherFunction() {
+
+  onMounted( () => {
     navigator.geolocation.getCurrentPosition(async (position) => {
-      const response = await fetch(`http://127.0.0.1:8000/met/nowcast?lat=${position.coords.latitude}&lon=${position.coords.longitude}`)
-      const resJson = await response.json()
-      console.log(JSON.stringify(resJson))
-      temp.value = resJson.properties.timeseries[0].data.instant.details.air_temperature
-      precipitation.value = resJson.properties.timeseries[0].data.next_1_hours.details.precipitation_amount
-      units.value = resJson.properties.meta.units
+      try {
+        const response = await fetch(`http://127.0.0.1:8000/met/nowcast?lat=${position.coords.latitude}&lon=${position.coords.longitude}`)
+        console.log(response.ok)
+        if (response.ok) {
+          responseCode.value = true
+          const resJson = await response.json()
+          console.log(JSON.stringify(resJson))
+          temp.value = resJson.properties.timeseries[0].data.instant.details.air_temperature
+          precipitation.value = resJson.properties.timeseries[0].data.next_1_hours.details.precipitation_amount
+          units.value = resJson.properties.meta.units
+          symbol.value = resJson.properties.timeseries[0].data.next_1_hours.summary.symbol_code
+          console.log(symbol.value)
+        }
+      } catch (error) {
+        console.log("Error: ", error)
+        responseCode.value = false
+      }
 
-      console.log(temp)
+
     })
-    
-  }
+  })
 
 
 </script>
