b527c4f4e2e052710307b923c74f57fffb1f9ac5
Tobias Lømo <tobias.lomo@gmail.com>
2023-07-04 15:00:57 +0200

    Added view to register a name

---
+++ src/views/onboarding/NameView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <input 
+      id="firstName"
+      type="text"
+      placeholder="First name"
+      :value="userStore.firstName"
+      @change="userStore.setFirstName(($event.target as HTMLInputElement).value)"
+    >
+    <input 
+      id="middleName"
+      type="text"
+      placeholder="Middle name"
+      :value="userStore.middleName"
+      @change="userStore.setMiddleName(($event.target as HTMLInputElement).value)"
+    >
+    <input 
+      id="LastName"
+      type="text"
+      placeholder="Last name"
+      :value="userStore.lastName"
+      @change="userStore.setLastName(($event.target as HTMLInputElement).value)"
+    >
+  </div>
+  
+</template>
+
+<script lang="ts" setup>
+import { useUserStore } from '@/stores/userStore';
+
+const userStore = useUserStore()
+
+</script>
