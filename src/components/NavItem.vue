e808662db369a6ed686a6a7f056b9d7b9b27c262
magnetenstad <tenstad.magne@gmail.com>
2023-07-04 15:57:37 +0200

    Highlight active tab

--- src/components/NavItem.vue
+++ src/components/NavItem.vue
@@ -1,12 +1,28 @@
 <template>
-  <div class="col center">
+  <div
+    class="nav-item col center"
+    :class="{ active: router.currentRoute.value.name == text.toLowerCase() }"
+  >
     <slot class="icon"></slot>
     <span>{{ text }}</span>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { router } from '@/router'
+
 defineProps<{
   text: string
 }>()
 </script>
+
+<style scoped>
+.nav-item {
+  padding: 0.5em;
+  width: 80px;
+}
+
+.active {
+  background-color: #00000010;
+}
+</style>
