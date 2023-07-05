6f593f9fc454ba6b423b940be4ffe67637aef7d2
magnetenstad <tenstad.magne@gmail.com>
2023-07-05 11:00:17 +0200

    Edit BirthdayView

--- src/views/onboarding/BirthdayView.vue
+++ src/views/onboarding/BirthdayView.vue
@@ -1,21 +1,21 @@
 <template>
-    <div class="view col">
-      <input 
-        id="birthday"
-        type="date"
-        :value="userStore.birthday?.toISOString().split('t')[0]"
-        @change="userStore.setBirthday(($event.target as HTMLInputElement).valueAsDate)"
-      >
-    </div>
-  
-    <button @click="router.push({ name: 'user' })">Done</button>
-  
-  </template>
-  
-  <script lang="ts" setup>
-  import { router } from '@/router';
-  import { useUserStore } from '@/stores/userStore';
-  
-  const userStore = useUserStore()
-  
-  </script>
+  <div class="view col">
+    <input
+      id="birthday"
+      type="date"
+      :value="userStore.birthday?.toISOString().split('T')[0]"
+      @blur="
+        userStore.setBirthday(($event.target as HTMLInputElement).valueAsDate)
+      "
+    />
+  </div>
+
+  <button @click="router.push({ name: 'user' })">Done</button>
+</template>
+
+<script lang="ts" setup>
+import { router } from '@/router'
+import { useUserStore } from '@/stores/userStore'
+
+const userStore = useUserStore()
+</script>
