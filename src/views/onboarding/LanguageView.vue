381230b3ebd214763e6352438a96fb3fde8d9701
Tobias Lømo <tobias.lomo@gmail.com>
2023-07-05 16:18:40 +0200

    Added buttons for languages

--- src/views/onboarding/LanguageView.vue
+++ src/views/onboarding/LanguageView.vue
@@ -2,6 +2,16 @@
     <div class="view col">
       <p>Select language</p>
     </div>
+
+    <div>
+      <input type="radio" id="norsk" name="language" value="Norsk">
+      <label for="norsk">Norsk</label><br>
+
+      <input type="radio" id="english" name="language" value="English">
+      <label for="english">English</label>
+
+
+    </div>
   
     <button @click="router.push({ name: 'user' })">Done</button>
   </template>
