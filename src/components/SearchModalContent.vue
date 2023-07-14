26f5e64114a4a10e5fb78bff05e766b63b4ab35b
magnetenstad <tenstad.magne@gmail.com>
2023-07-14 10:59:32 +0200

    Focus on search

--- src/components/SearchModalContent.vue
+++ src/components/SearchModalContent.vue
@@ -3,8 +3,9 @@
     <h3 style="text-align: center">{{ $t().searchAddress }}</h3>
     <input
       type="text"
+      ref="inputRef"
       id="street-address"
-      :placeholder="place.address.street"
+      :placeholder="place.address.street || '...'"
       @click="search"
       @input="(event) => {
         place.address.street = (event.target as HTMLInputElement).value;
@@ -32,16 +33,21 @@ import {
   AddressResult,
 } from '@/stores/placeStore'
 import { $t } from '@/translation'
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 
 const props = defineProps<{ place?: Place }>()
-const place = ref<Place>(props.place ?? defaultPlace())
 
 const emit = defineEmits<{
   (e: 'select', place: Place): void
 }>()
 
+const inputRef = ref<HTMLInputElement | undefined>()
+const place = ref<Place>(props.place ?? defaultPlace())
 const results = ref<AddressResult[]>([])
+
+onMounted(() => {
+  inputRef.value?.focus()
+})
 
 const search = () => {
   searchAddress(place.value.address, (r) => (results.value = r))
